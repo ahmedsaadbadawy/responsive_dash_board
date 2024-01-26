dc8607666ac53e322f3bca27876efade10858c3e
ahmedsaadbadawy <ahmedzack168@gmail.com>
2024-01-26 23:29:16 +0200

    finish custom drawer

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/models/drawer_item_model.dart';
 import 'package:responsive_dash_board/utils/app_images.dart';
+import 'package:responsive_dash_board/widgets/active_and_inactive_drawer_item.dart';
 import 'package:responsive_dash_board/widgets/user_info_list_tile.dart';
+
+import 'drawer_items_list_view.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
@@ -9,13 +13,48 @@ class CustomDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: const Column(
-        children: [
-          UserInfoListTile(
-            image: Assets.imagesAvatar1,
-            title: 'LeKan OKeowo',
-            subTitle: 'demo@gmail.com',
-          )
+      child: const CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: UserInfoListTile(
+              image: Assets.imagesAvatar1,
+              title: 'LeKan OKeowo',
+              subTitle: 'demo@gmail.com',
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: SizedBox(
+              height: 8,
+            ),
+          ),
+          DrawerItemsListView(),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              children: [
+                Expanded(
+                  child: SizedBox(
+                    height: 20,
+                  ),
+                ),
+                InActiveDrawerItem(
+                  drawerItemModel: DrawerItemModel(
+                    title: 'Settings',
+                    image: Assets.imagesSettings,
+                  ),
+                ),
+                InActiveDrawerItem(
+                  drawerItemModel: DrawerItemModel(
+                    title: 'Logout account',
+                    image: Assets.imagesLogout,
+                  ),
+                ),
+                SizedBox(
+                  height: 48,
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
---
+++ lib/widgets/drawer_items_list_view.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import '../models/drawer_item_model.dart';
+import '../utils/app_images.dart';
+import 'drawer_item.dart';
+
+class DrawerItemsListView extends StatefulWidget {
+  const DrawerItemsListView({
+    super.key,
+  });
+
+  @override
+  State<DrawerItemsListView> createState() => _DrawerItemsListViewState();
+}
+
+class _DrawerItemsListViewState extends State<DrawerItemsListView> {
+  int activeIndex = 0;
+
+  final List<DrawerItemModel> items = [
+    const DrawerItemModel(title: 'Dashboard', image: Assets.imagesDashboard),
+    const DrawerItemModel(
+        title: 'My Transactions', image: Assets.imagesMyTransctions),
+    const DrawerItemModel(title: 'Statistics', image: Assets.imagesStatistics),
+    const DrawerItemModel(
+        title: 'Wallet Account', image: Assets.imagesWalletAccount),
+    const DrawerItemModel(
+        title: 'My Investments', image: Assets.imagesMyInvestments),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return SliverList.builder(
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          onTap: () {
+            setState(() {
+              if (activeIndex != index) {
+                activeIndex = index;
+              }
+            });
+          },
+          child: Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: DrawerItem(
+              drawerItemModel: items[index],
+              isActive: activeIndex == index,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
