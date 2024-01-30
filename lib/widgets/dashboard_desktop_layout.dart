8078d8511f4c243e98b3db7f883d755e60799299
ahmedsaadbadawy <ahmedzack168@gmail.com>
2024-01-30 21:52:39 +0200

    add quick invoice header

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/all_expenses.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
+import 'package:responsive_dash_board/widgets/quick_invoice.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
@@ -19,9 +20,11 @@ class DashboardDesktopLayout extends StatelessWidget {
           flex: 2,
           child: Column(
             children: [
-              Expanded(
-                child: AllExpenses(),
+              AllExpenses(),
+              SizedBox(
+                height: 24,
               ),
+              QuickInvoice(),
             ],
           ),
         ),
