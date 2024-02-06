17465f4219b7f853ea99f10d1276497ecc98a725
ahmedsaadbadawy <ahmedzack168@gmail.com>
2024-02-06 20:42:07 +0200

    responsive mobile layout (app done)

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -3,15 +3,39 @@ import 'package:responsive_dash_board/widgets/adaptive_layout_widget.dart';
 import 'package:responsive_dash_board/widgets/dashboard_mobile_layout.dart';
 import 'package:responsive_dash_board/widgets/dashboard_tablet_layout.dart';
 
+import '../utils/size_config.dart';
+import '../widgets/custom_drawer.dart';
 import '../widgets/dashboard_desktop_layout.dart';
 
-class DashBoardView extends StatelessWidget {
+class DashBoardView extends StatefulWidget {
   const DashBoardView({super.key});
 
   @override
+  State<DashBoardView> createState() => _DashBoardViewState();
+}
+
+class _DashBoardViewState extends State<DashBoardView> {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
+  @override
   Widget build(BuildContext context) {
+    SizeConfig.init(context);
     return Scaffold(
+      key: scaffoldKey,
+      appBar: MediaQuery.sizeOf(context).width < SizeConfig.tablet
+          ? AppBar(
+              elevation: 0,
+              backgroundColor: const Color(0xFFFAFAFA),
+              leading: IconButton(
+                  onPressed: () {
+                    scaffoldKey.currentState!.openDrawer();
+                  },
+                  icon: const Icon(Icons.menu)),
+            )
+          : null,
       backgroundColor: const Color(0xFFFAFAFA),
+      drawer: MediaQuery.sizeOf(context).width < SizeConfig.tablet
+          ? const CustomDrawer()
+          : null,
       body: AdaptiveLayout(
         mobileLayout: (context) => const DashBoardMobileLayout(),
         tabletLayout: (context) => const DashBoardTabletLayout(),
--- lib/widgets/inactive_and_active_all_expenses_item.dart
+++ lib/widgets/inactive_and_active_all_expenses_item.dart
@@ -31,23 +31,32 @@ class InActiveAllExpensesItem extends StatelessWidget {
           const SizedBox(
             height: 34,
           ),
-          Text(
-            itemModel.title,
-            style: AppStyles.styleMedium16(context),
+          FittedBox(
+            fit: BoxFit.scaleDown,
+            child: Text(
+              itemModel.title,
+              style: AppStyles.styleMedium16(context),
+            ),
           ),
           const SizedBox(
             height: 8,
           ),
-          Text(
-            itemModel.date,
-            style: AppStyles.styleRegular14(context),
+          FittedBox(
+            fit: BoxFit.scaleDown,
+            child: Text(
+              itemModel.date,
+              style: AppStyles.styleRegular14(context),
+            ),
           ),
           const SizedBox(
             height: 16,
           ),
-          Text(
-            itemModel.price,
-            style: AppStyles.styleSemiBold24(context),
+          FittedBox(
+            fit: BoxFit.scaleDown,
+            child: Text(
+              itemModel.price,
+              style: AppStyles.styleSemiBold24(context),
+            ),
           ),
         ],
       ),
@@ -70,6 +79,7 @@ class ActiveAllExpensesItem extends StatelessWidget {
       decoration: ShapeDecoration(
         color: const Color(0xFF4DB7F2),
         shape: RoundedRectangleBorder(
+          side: const BorderSide(width: 1, color: Color(0xFF4DB7F2)),
           borderRadius: BorderRadius.circular(12),
         ),
       ),
@@ -77,34 +87,44 @@ class ActiveAllExpensesItem extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           AllExpensessItemHeader(
-            imageBackground: Colors.white.withOpacity(0.10000000149011612),
+            imageBackground: Colors.white.withOpacity(0.1),
             imageColor: Colors.white,
             image: itemModel.image,
           ),
           const SizedBox(
             height: 34,
           ),
-          Text(
-            itemModel.title,
-            style: AppStyles.styleMedium16(context).copyWith(
-              color: Colors.white,
+          FittedBox(
+            fit: BoxFit.scaleDown,
+            child: Text(
+              itemModel.title,
+              style: AppStyles.styleMedium16(context).copyWith(
+                color: Colors.white,
+              ),
             ),
           ),
           const SizedBox(
             height: 8,
           ),
-          Text(
-            itemModel.date,
-            style: AppStyles.styleSemiBold24(context).copyWith(
-              color: const Color(0xFFFAFAFA),
+          FittedBox(
+            fit: BoxFit.scaleDown,
+            child: Text(
+              itemModel.date,
+              style: AppStyles.styleRegular14(context).copyWith(
+                color: const Color(0xFFFAFAFA),
+              ),
             ),
           ),
           const SizedBox(
             height: 16,
           ),
-          Text(
-            itemModel.price,
-            style: AppStyles.styleSemiBold24(context).copyWith(color: Colors.white),
+          FittedBox(
+            fit: BoxFit.scaleDown,
+            child: Text(
+              itemModel.price,
+              style: AppStyles.styleSemiBold24(context)
+                  .copyWith(color: Colors.white),
+            ),
           ),
         ],
       ),
